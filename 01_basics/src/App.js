f35495219304b8168c4bd37eddcbd88beda1d354
Parthib Sarkar <parthibsid5@gmail.com>
2024-04-21 12:13:23 +0530

    custom react and vitereact added

---
+++ 01_basics/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1>
+      This is the manual react app creation | Parthib Sarkar
+    </h1>
+  )
+}
+
+export default App;
---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Started react using vite | Parthib Sarkar</h1>
+  )
+}
+
+export default App;
